0f2df463a487657aeae82988de545bb06b3c9084
Jeff Kilroy <jeffkilroy@gmail.com>
2015-08-03 10:08:03 -0400

    Selectable category taxonomy

--- jk-comics.php
+++ jk-comics.php
@@ -17,7 +17,14 @@ $wpDeveloper = new WPDeveloper();
 $wpDeveloper->verify();
 
 //Create Post Type
-add_action( 'init', 'jkcomics_create_comic_post_type' );
+add_action( 'init', 'jkcomics_init' );
+function jkcomics_init(){
+    jkcomics_create_comic_post_type();
+    jkcomics_create_comic_type_taxonomy();
+    //Admin Page
+    add_action( 'admin_menu', 'jkcomics_plugin_menu' );
+}
+
 function jkcomics_create_comic_post_type(){
     $postType = new PostType();
     $postType->name = 'jkcomic';
@@ -27,4 +34,29 @@ function jkcomics_create_comic_post_type(){
     $postType->excerptTitle = 'Description';
     $postType->excerptHelp = 'Comic Alt Text / Description';
     $postType->create();
+}
+
+function jkcomics_create_comic_type_taxonomy(){
+    $taxonomy = new Taxonomy();
+    $taxonomy->post_type = 'jkcomic';
+    $taxonomy->name = 'comic_types';
+    $taxonomy->urlSlug = 'comic-type';
+    $taxonomy->labelPlural = 'Comic Types';
+    $taxonomy->labelSingular = 'Comic Type';
+    $taxonomy->create();
+}
+
+//Menu
+function jkcomics_plugin_menu() {
+    add_options_page( 'JK Comic Import', 'JK Comics Import', 'manage_options', 'jk-comics-import', 'jkcomics_import_page' );
+}
+
+//Import Page
+function jkcomics_import_page() {
+    if ( !current_user_can( 'manage_options' ) )  {
+        wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+    }
+    echo '<div class="wrap">';
+    echo '<p>Here is where the form would go if I actually had options.</p>';
+    echo '</div>';
 }
